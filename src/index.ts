884cfc6ee61722b923cd48501a9d2f3a36d26d8d
Eddie <edd.coder@gmail.com>
2025-09-13 14:06:30 -0500

    Updates package version and exports types
    Bumps the package version to 0.1.2.
    
    Exports `NexusEvent` and `NexusEventListener` types, and
    `NexusEventManager` class for external use.

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export * from './models/NexusEvent.model';
-export * from './models/NexusEventListener.model';
+export type { NexusEvent } from './models/NexusEvent.model';
+export type { NexusEventListener } from './models/NexusEventListener.model';
 
 export { default as NexusEventManager } from './event.manager';
